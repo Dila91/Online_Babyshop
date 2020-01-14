b5afa73a65b947899d10a036856e2ff90b7b60c0
arwa lokat <arwa533@hotmail.com>
2020-01-14 14:07:05 +0100

    initial commit

---
+++ Online_Babyshop/Data/Migrations/20200114082536_initia.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Online_Babyshop.Data.Migrations
+{
+    public partial class initia : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
